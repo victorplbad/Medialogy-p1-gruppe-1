4b9e819ae95c9040a406e9142daae361d565cded
koldefoedder <koldefoedder@outlook.com>
2025-11-25 13:05:00 +0100

    Timer + gameDoneButton Prefab

--- Assets/Scripts/TimeScript.cs
+++ Assets/Scripts/TimeScript.cs
@@ -2,6 +2,10 @@ using UnityEngine;
 
 public class TimeScript : MonoBehaviour
 {
+    public float timer = 0f;
+    public float debrisSec = 0.64f;
+    public float debrisTotal = 0f;
+    public bool gameDone = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +15,13 @@ public class TimeScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        timer += Time.deltaTime;
     }
+
+    public void gameDoneButton()
+    {
+        debrisTotal = timer * debrisSec; 
+        Debug.Log(debrisTotal);
+    }
+
 }
