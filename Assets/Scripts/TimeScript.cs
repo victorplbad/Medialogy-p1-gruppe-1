58f84c1e58c3d66bae5a62be437f812f061bbdcb
victorplbad <84019887+victorplbad@users.noreply.github.com>
2025-11-25 15:16:20 +0100

    fixes many prefabs

--- Assets/Scripts/TimeScript.cs
+++ Assets/Scripts/TimeScript.cs
@@ -5,7 +5,6 @@ public class TimeScript : MonoBehaviour
     public float timer = 0f;
     public float debrisSec = 0.64f;
     public float debrisTotal = 0f;
-    public bool gameDone = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
