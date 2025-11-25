69ca5a3fd71de62a7934ca051153b5c98702a0c7
victorplbad <84019887+victorplbad@users.noreply.github.com>
2025-11-25 12:09:21 +0100

    qunstion ui basic + test

---
+++ Assets/Scripts/notebookchecks.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class notebookchecks : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    public GameObject checkbox;
+    public GameObject starbox;
+
+    public bool rightAnswer = false;
+
+
+    public void Right(bool correct)
+    {
+        rightAnswer = correct;
+
+    }
+
+
+    public void ButtonPress()
+    {
+
+
+        if (rightAnswer == true)
+        {
+
+            starbox.SetActive(true);
+
+        }
+
+        checkbox.SetActive(true);
+
+    }
+
+
+}
