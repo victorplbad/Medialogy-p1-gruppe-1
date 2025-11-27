783b20b734fb614fe36761e2bf9724eb0217be1f
CasperLarsen-cmyk <casper.larsen.a@gmail.com>
2025-11-27 10:17:37 +0100

    radar code cleanup

--- Assets/Scripts/RadarArrow.cs
+++ Assets/Scripts/RadarArrow.cs
@@ -1,4 +1,3 @@
-using System.Threading;
 using UnityEngine;
 
 public class RadarArrow : MonoBehaviour
@@ -11,28 +10,29 @@ public class RadarArrow : MonoBehaviour
     {
         if (!origin) return;
 
-        GameObject closestObj = null;
+        GameObject closestObject = null;
         float range = 9999999;                                                                          //Big number that will be larger then whatever real objects distance
-        GameObject[] things = GameObject.FindGameObjectsWithTag(targetTag);
 
-        for (int i = 0; i < things.Length; i++)
+        GameObject[] objects = GameObject.FindGameObjectsWithTag(targetTag);
+
+        for (int i = 0; i < objects.Length; i++)
         {
-            float iRange = (things[i].transform.position - origin.transform.position).magnitude;
+            float iRange = (objects[i].transform.position - origin.transform.position).magnitude;
             if (iRange < range)
             {
-                closestObj = things[i];
+                closestObject = objects[i];
                 range = iRange;
             }
         }
 
-        if (!closestObj) return;
+        if (!closestObject) return;
 
         //Vector3 direction = origin.transform.InverseTransformPoint(closestObj.transform.position);    //Gets angle from boat perspective
-        Vector3 direction = closestObj.transform.position - origin.transform.position;                  //Gets angle from absolute posistions
-        float angle = -Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+        Vector3 direction = closestObject.transform.position - origin.transform.position;               //Gets angle from absolute posistions
+        float angle = Mathf.Atan2(-direction.x, direction.z) * Mathf.Rad2Deg;
 
-        //radarArrow.transform.localEulerAngles = new Vector3(0, 0, angle);
         //radarArrow.transform.localEulerAngles.Set(0, 0, angle);//Y NO WORK
+        //radarArrow.transform.localEulerAngles = new Vector3(0, 0, angle);
 
         float angleDiff = radarArrow.transform.localEulerAngles.z - angle;
         if (angleDiff > 180) angleDiff -= 360;
