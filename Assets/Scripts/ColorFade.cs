dbeeef3d7fb71735d89055065f6165536c57d998
CasperLarsen-cmyk <casper.larsen.a@gmail.com>
2025-11-27 15:25:30 +0100

    Added backgroudn to question and made the prefabs use reect transform

--- Assets/Scripts/ColorFade.cs
+++ Assets/Scripts/ColorFade.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,20 +10,25 @@ public class ColorFade : MonoBehaviour
     public bool destroyObjectAtEnd = true;
 
     private Image image;
+    private TextMeshProUGUI text;
     private float timer;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         image = GetComponent<Image>();
+        if (transform.childCount > 0) text = transform.GetChild(0).GetComponent<TextMeshProUGUI>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        timer += Time.deltaTime / time;
+        timer += Time.unscaledDeltaTime / time;
+        Color color = Color.Lerp(startColor, endColor, timer);
 
-        image.color = Color.Lerp(startColor, endColor, timer);
+        if (image) image.color = color;
+        if (text) text.color = new Color(0, 0, 0, color.a);
+
         if (destroyObjectAtEnd && timer > 1) Destroy(this.gameObject);  //Destroy host gameObject
         else if (timer > 1) Destroy(this);                              //Destroy only the script
     }
