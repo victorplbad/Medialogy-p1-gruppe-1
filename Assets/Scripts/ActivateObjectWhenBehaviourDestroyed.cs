30a6d20c61dd31619f2cdfe9036086c0f87dce43
CasperLarsen-cmyk <casper.larsen.a@gmail.com>
2025-12-08 10:57:22 +0100

    refixed it

--- Assets/Scripts/ActivateObjectWhenBehaviourDestroyed.cs
+++ Assets/Scripts/ActivateObjectWhenBehaviourDestroyed.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 public class ActivateObjectWhenBehaviourDestroyed : MonoBehaviour
 {
     public MonoBehaviour behaviour;
-    public GameObject gameObject;
+    public GameObject go;
 
     // Update is called once per frame
     void Update()
     {
         if (behaviour.IsDestroyed())
         {
-            gameObject.SetActive(behaviour.IsDestroyed());
+            go.SetActive(behaviour.IsDestroyed());
             Destroy(this);
         }
     }
