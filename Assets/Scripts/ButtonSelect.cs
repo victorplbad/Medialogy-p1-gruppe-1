3f54eed1defb5bcd0fa19fa1f6ec4e0d779498f3
CasperLarsen-cmyk <casper.larsen.a@gmail.com>
2025-12-08 14:01:23 +0100

    Added initial button selection main

---
+++ Assets/Scripts/ButtonSelect.cs
@@ -0,0 +1,36 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ButtonSelect : MonoBehaviour
+{
+    void Start()
+    {
+        List<Button> list = new List<Button>();
+        for (int i = 0; i < transform.childCount; i++)
+        {
+
+            Button b = transform.GetChild(i).GetComponent<Button>();
+
+            if (b == null) continue;
+
+            list.Add(b);
+        }
+
+        Button topButton = list[0];
+        for (int i = 0;i < list.Count; i++) {
+            if (list[i].transform.position.y > topButton.transform.position.y) topButton = list[i];
+        }
+
+        EventSystem.current.SetSelectedGameObject(topButton.gameObject);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            EventSystem.current.currentSelectedGameObject.GetComponent<Button>().onClick.Invoke();
+        }
+    }
+}
